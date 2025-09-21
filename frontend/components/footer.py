77e3188276417e9168947dc386d2e582dc504249
Raushan kumar <raushankumar321670@gmail.com>
2025-09-21 11:18:13 +0530

    Update footer.py

--- frontend/components/footer.py
+++ frontend/components/footer.py
@@ -4,6 +4,7 @@ def render_footer():
     st.markdown("""
         <hr style="margin-top: 40px;">
         <div style="text-align: center; font-size: 14px; color: gray;">
-            Built with ❤️ by Team Solo Leveler | <a href="https://github.com/rkmaurya93049" target="_blank">GitHub</a>
+            Built with ❤️ by Team Solo Leveler | <a href="https://github.com/rkmaurya93049/SkillSyncer" target="_blank">GitHub</a>
         </div>
+
     """, unsafe_allow_html=True)

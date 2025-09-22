4bc2596898981b294427fb57c3b28f47bffbb063
Raushan kumar <raushankumar321670@gmail.com>
2025-09-22 12:13:36 +0530

    Update dashboard.py

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -17,7 +17,7 @@ render_header()
 tab = st.selectbox("🔀 Switch View", ["🎓 Student Portal", "🧑‍💼 Recruiter Dashboard"])
 
 # --- Backend URL ---
-BASE_URL = "http://0.0.0.0:7860"
+BASE_URL = "https://skillsyncer-rkmaurya.hf.space"
 
 
 
@@ -169,6 +169,7 @@ elif "Recruiter" in tab:
 
 # --- Footer ---
 render_footer()
+
 
 
 
